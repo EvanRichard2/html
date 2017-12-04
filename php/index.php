ade66b64eb505c9c1fd2de4ab18b4262cccb58e2
Lelouch Lamperouge <31546491+EvanRichard2@users.noreply.github.com>
2017-12-04 09:20:07 -0800

    Add files via upload

--- php/index.php
+++ php/index.php
@@ -1,243 +1,53 @@
-<!DOCTYPE html>
-<html><title>0000 1010 . 1000 0001 . 0000 0001 . 0001 0010</title>
+<html>
 <head>
+<title> Evan HTML </title>
+<p style="text-align:center;color: white;"> HTML (Hyper Text Markup Language) ASSIGNMENTS </p><br />
 <style>
-/*
-navbar
-*/
-body {margin:0;}
-
-.topnav {
-	font-family: Arial, san-serif;
-  overflow: hidden;
-  background-color: #333;
+body {
+	background-image: url("img/Smile.png");
+    background-attachment: fixed;
+    background-position: center; 
 }
-
-.topnav a {
-  float: left;
-  display: block;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
+.a{
+	display:inline-block;
 }
-
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-.topnav .icon {
-  display: none;
-}
-/*
-end navbar
-custom
-*/
-.fbox {
-	font-family: Arial, san-serif;
-	font-style: bold;
-	font-size: 24px;
-	color: #eee;
-	font-family: Arial, san-serif;
+a{
+    color: white;
+    text-align: center;
+    text-decoration: none;
     display: inline-block;
-    text-align: center;
-    width: 30px;
-    height:30px;
-    margin: 3px;
-    border: 1px solid #00a500;
-    background-color: #00a500;  
-}
-.tbox {
-	font-family: Arial, san-serif;
-	font-style: bold;
-	font-size: 24px;
-	color: #00a500;
-	font-family: Arial, san-serif;
-    display: inline-block;
-    text-align: center;
-    width: 30px;
-    height:30px;
-    margin: 3px;
-    border: 1px solid #00a500;
-    background-color: #fff;  
-}
-.abox {
-    border: 3px solid green; 
-}
-
-a {
-	font-family: Arial, san-serif;
-	font-style: bold;
-	font-size: 24px;
-text-decoration : none;	
-	color: #fff;
-}
-
-a:hover{
-	
-color: #000;	
-	
-}
-/*
- media responsive
-*/
-* {
-    box-sizing: border-box;
-}
-.row::after {
-    content: "";
-    clear: both;
-    display: block;
-}
-[class*="col-"] {
-    float: left;
-    padding: 15px;
-}
-html {
-    font-family: "Lucida Sans", sans-serif;
-}
-.header {
-    background-color: #9933cc;
-    color: #ffffff;
-    padding: 15px;
-}
-.menu ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-}
-.menu li {
-    padding: 8px;
-    margin-bottom: 7px;
-    background-color: #33b5e5;
-    color: #ffffff;
-    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-}
-.menu li:hover {
-    background-color: #0099cc;
-}
-.aside {
-    background-color: #33b5e5;
-    padding: 15px;
-    color: #ffffff;
-    text-align: center;
-    font-size: 14px;
-    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-}
-.footer {
-    background-color: #0099cc;
-    color: #ffffff;
-    text-align: center;
-    font-size: 12px;
-    padding: 15px;
-}
-/* For desktop: */
-.col-1 {width: 8.33%;}
-.col-2 {width: 16.66%;}
-.col-3 {width: 25%;}
-.col-4 {width: 33.33%;}
-.col-5 {width: 41.66%;}
-.col-6 {width: 50%;}
-.col-7 {width: 58.33%;}
-.col-8 {width: 66.66%;}
-.col-9 {width: 75%;}
-.col-10 {width: 83.33%;}
-.col-11 {width: 91.66%;}
-.col-12 {width: 100%;}
-
-/*
-end media responsive
-
-*/
-
-@media screen and (max-width: 600px) {
-  .topnav a:not(:first-child) {display: none;}
-  .topnav a.icon {
-    float: right;
-    display: block;
-  }
-  [class*="col-"] {
-        width: 100%;
-    }
-}
-
-@media screen and (max-width: 600px) {
-  .topnav.responsive {position: relative;}
-  .topnav.responsive .icon {
-    position: absolute;
-    right: 0;
-    top: 0;
-  }
-  .topnav.responsive a {
-    float: none;
-    display: block;
-    text-align: left;
-  }
 }
 </style>
 </head>
 <body>
-
-<div class="topnav" id="myTopnav">
-  <a href="#home">Home</a>
-  <a href="html/">html</a>
-  <a href="downloads/">downloads</a>
-  <a href="phpform/">phpform</a>
-  <a href = "https://www.netacad.com/courses/packet-tracer-download/" target = "_blank">Packet Tracer Register</a>
-  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+<div class="a">
+	<p style="color: white;">GitHub</p>
 </div>
-&nbsp; &nbsp;0000 1010 . 1011 0111 . 0000 0001 . 0001 0010
-<div class="row">
-	<div class="col-4">
-		<div class = "tbox">A</div><div class = "tbox">M</div><br />
-			<?php
-				for ($i = 1; $i < 21; $i++){
-						echo "<div class=\"fbox\">";
-						$am = "http://10.183.1.";
-						echo "<a href =\"".$am.$i."\" target = \"_blank\" >".$i."</a>";
-						echo "</div>";
-						if ($i != 0 && $i % 6 == 0 && $i < 17) echo "<br />";
-					if ($i == 16) echo "<br />";
-					}
-			?>
-	</div>
-	
-<div class = "col-2">
-	<div class = "fbox"><a href = "http://10.183.1.26" target = "_blank"> 26 </a></div>
-	<div class = "fbox"><a href = "http://10.183.1.30" target = "_blank"> 30 </a></div>
+<div class="a">
+	<p style="color: white;">CodeCademy</p>
+</div>
+<div><a href="https://github.com/EvanRichard2" > <img src="img/images.png" 
+		style="width:100px;height:100px;"> </a>
+	<a href="https://www.codecademy.com/learn" > <img src="img/code.png" 
+		style="width:100px;height:100px;"> </a>
 </div>
 
-	<div class="col-4">
-	  <div class = "tbox">P</div><div class = "tbox">M</div><br />
-		<?php
-			for ($i = 1; $i < 21; $i++){
-					echo "<div class=\"fbox\">";
-					$am = "http://10.183.2.";
-					echo "<a href =\"".$am.$i."\" target = \"_blank\" >".$i."</a>";
-					echo "</div>";
-					if ($i != 0 && $i % 6 == 0 && $i < 17) echo "<br />";
-				if ($i == 16) echo "<br />";
-				}
-		?>
-	</div>
-</div>
+
+	<hr />	
+<div class="a"><a href = "html/"> Html </a></div>
 <hr />
-	<div class = "fbox"><a href = "https://www.nitrotype.com/" target = "_blank"> nitrotype</a></div>
-	<div class = "fbox"><a href = "http://thetypingcat.com/" target = "_blank"> the typing cat </a></div>
-	
+<a href = "logs/"> Logs </a>
+<hr />
+<a href="http://icebowl.github.io" >Icebowl.github.io</a>
+<hr />
+<a href="https://evanrichard2.github.io/" >Evanrichard2.github.io</a>
+<hr />
+<a href="http://tritechsc.ksd.org/cyber/assignments/" >Assignments</a>
+<hr />
+<a href="python/" >Python</a>
+<hr />
+<a href="mdb/" >Mdb</a>
+<hr />
 
-
-<script>
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-</script>
 </body>
 </html>
